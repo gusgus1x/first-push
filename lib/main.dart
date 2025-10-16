629c429447cc588659ed3f01c886a0c1e7ab4f56
Sathorn Kamalasin <1166109060028@mail.rmutt.ac.th>
2025-10-16 11:41:26 +0700

    update: แก้ไขข้อความเป็นชื่อตัวเอง

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Sathorn Kamalasin'),
+          child: Text('โค้ดของสาทร'),
         ),
       ),
     );
